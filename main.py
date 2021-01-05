7f43a02b7a21fc913fd783ac84284ee2354e4a14
Alperen Ekin <alperenekin97@gmail.com>
2021-01-05 22:51:30 +0300

    Can get the probability of divisor correct

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import numpy as np
+from itertools import product
+import copy
+
 
 
 def find_solution(data):
@@ -35,13 +38,76 @@ def find_independent_prob(p_a, p_b, p_c, p_d, p_e, p_f, p_g, data):
     p_e /= len(data[0])
     p_f /= len(data[0])
     p_g /= len(data[0])
+def mainEquation(a, b, c, d, e, f, g):
+    equation_dict = {
+        "A": p_a,
+        "B": p_b,
+        "nA": 1 - p_a,
+        "nB": 1 - p_b,
+        "CA": c_a,
+        "nCA": not_c_a,
+        "CnA": c_not_a,
+        "nCnA": not_c_not_a,
+        "GD": g_d,
+        "nGD": not_g_d,
+        "GnD": g_not_d,
+        "nGnD": not_g_not_d,
+        "FC": f_c,
+        "nFC": not_f_c,
+        "FnC": f_not_c,
+        "nFnC": not_f_not_c,
+        "EC": e_c,
+        "nEC": not_e_c,
+        "EnC": e_not_c,
+        "nEnC": not_e_not_c,
+        "DAB": d_a_b,
+        "nDAB": not_d_a_b,
+        "DnAB": d_not_a_b,
+        "DAnB": d_a_not_b,
+        "DnAnB": d_not_a_not_b,
+        "nDnAB": not_d_not_a_b,
+        "nDAnB": not_d_a_not_b,
+        "nDnAnB": not_d_not_a_not_b
+    }
+    print(b)
+    result = equation_dict[a] * equation_dict[b] * equation_dict[c + a] * equation_dict[e + c] * equation_dict[g + d] * equation_dict[c + a] * equation_dict[d + a + b] * equation_dict[f + c]
+
+    return result
+
+
+def sortVariableList(given_list, missing_list):
+    for item in given_list:
+        missing_list.append(item)
+
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(missing_list) - 1):
+            if len(missing_list[i]) == 1 and len(missing_list[i + 1]) == 1:
+                if missing_list[i] > missing_list[i + 1]:
+                    missing_list[i], missing_list[i + 1] = missing_list[i + 1], missing_list[i]
+                    swapped = True
+            if len(missing_list[i]) == 2 and len(missing_list[i + 1]) == 1:
+                if missing_list[i][1] > missing_list[i + 1]:
+                    missing_list[i], missing_list[i + 1] = missing_list[i + 1], missing_list[i]
+                    swapped = True
+            if len(missing_list[i]) == 1 and len(missing_list[i + 1]) == 2:
+                if missing_list[i] > missing_list[i + 1][1]:
+                    missing_list[i], missing_list[i + 1] = missing_list[i + 1], missing_list[i]
+                    swapped = True
+            if len(missing_list[i]) == 2 and len(missing_list[i + 1]) == 2:
+                if missing_list[i][1] > missing_list[i + 1][1]:
+                    missing_list[i], missing_list[i + 1] = missing_list[i + 1], missing_list[i]
+                    swapped = True
+    print(missing_list)
+    return missing_list
 
 
 if __name__ == '__main__':
     arr = np.load('data 1.npy')
     number_of_data = len(arr[0])
 
-    p_a = 0
+    p_a = 0  # TODO make them one line
     p_b = 0
     p_c = 0
     p_d = 0
@@ -65,6 +131,7 @@ if __name__ == '__main__':
     d_and_not_a_b = 0
     d_and_a_not_b = 0
     d_and_not_a_not_b = 0
+
     p_a_b = 0
     p_a_not_b = 0
     p_not_a_b = 0
@@ -157,4 +224,52 @@ if __name__ == '__main__':
     p_f /= number_of_data
     p_g /= number_of_data
     print(p_g)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+    variable_dict = {
+        "A": p_a,
+        "B": p_b,
+        "C": p_c,
+        "D": p_d,
+        "E": p_e,
+        "F": p_f,
+        "G": p_g,
+        "nA": 1 - p_a,
+        "nB": 1 - p_b,
+        "nC": 1 - p_c,
+        "nD": 1 - p_d,
+        "nE": 1 - p_e,
+        "nF": 1 - p_f,
+        "nG": 1 - p_g,
+    }
+
+    pay = ["A", "C"]
+    payda = ["A", "C"]
+    pay_list = []
+    pay_dict = {}
+    for item in pay:
+        if len(item) == 1:
+            pay_dict[item] = variable_dict[item]
+            pay_dict["n" + item] = variable_dict["n" + item]
+            del variable_dict[item]
+            del variable_dict["n" + item]
+        else:
+            pay_dict[item] = variable_dict[item]
+            pay_dict[item[1]] = variable_dict[item[1]]
+            del variable_dict[item]
+            del variable_dict[item[1]]
+
+    for missing_variables in list(variable_dict.keys()):
+        if len(missing_variables) == 1:
+            small_list = [missing_variables, "n" + missing_variables]
+            pay_list.append(small_list)
+
+    print(pay_list)
+    print(list(variable_dict.keys()))
+
+    a = 0
+    for elements in product(pay_list[0]):
+        print(list(elements))
+        mylist = sortVariableList(list(elements), copy.deepcopy(pay))
+        a += mainEquation(mylist[0], mylist[1], mylist[2], mylist[3], mylist[4], mylist[5], mylist[6])
+
+    print(a)
